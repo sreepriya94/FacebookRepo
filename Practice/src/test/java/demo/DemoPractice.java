42ec132351ccd929de2250fe79a9da9745370647
sreep <sreep@SreepriyaLap>
2023-11-10 14:53:51 +0530

    this is the fist commit

---
+++ Practice/src/test/java/demo/DemoPractice.java
@@ -0,0 +1,20 @@
+package demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DemoPractice {
+	@Test
+public void practiceMethod()
+{
+	WebDriverManager.chromedriver().setup();
+	ChromeOptions options=new ChromeOptions();
+	options.addArguments("--remote-allow-origins=*");
+	WebDriver driver=new ChromeDriver(options);
+	driver.get("https://www.facebook.com/");
+}
+}
